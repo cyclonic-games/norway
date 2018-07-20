1cd97367662242b64acb11aca205074f5b8c86ce
Nick Dugger <email@nickdugger.com>
2018-07-20 12:16:35 -0500

    Window dimensions

--- plugins/common/index.js
+++ plugins/common/index.js
@@ -2,6 +2,6 @@ const Plugin = require('quantum/core/Plugin');
 
 module.exports = new Plugin('common', host => {
     host.subscribe('start').forEach(() => {
-        host.ui.render('main', { width: 1024, height: 768, frame: false, transparent: true, webPreferences: { webSecurity: false } });
+        host.ui.render('main', { width: 1440, height: 900, frame: false, transparent: true, webPreferences: { webSecurity: false } });
     });
 });
